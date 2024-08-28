5100e4f838be673afc61547bb8a9c41d8074a0ee
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2024-08-29 00:03:22 +0600

    test update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,6 +1,7 @@
 @extends('frontend.layouts.master')
 @section('title', 'Best online shopping in Bangladesh')
 @section('content')
+ppp
     <main class="main" id="bg">
         <div class="container-fluide" style="position: relative;">
             <div class="row">
